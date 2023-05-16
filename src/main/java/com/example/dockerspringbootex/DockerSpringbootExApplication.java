c78a741650a4f100da49f568de2534687e2daebe
dustyooyu <somecalm@gmail.com>
2023-05-16 14:36:32 +0900

    feat: project build

---
+++ src/main/java/com/example/dockerspringbootex/DockerSpringbootExApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringbootExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringbootExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerspringbootex/DockerSpringbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringbootExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
